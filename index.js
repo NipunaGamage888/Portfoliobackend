aed8ff9ac25d568da62d279e6e8a9b554ccefd29
NipunaGamage888 <69056579+NipunaGamage888@users.noreply.github.com>
2024-05-16 10:35:01 -0400

    blehhh

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Portfolio Backend!');
+});
+
 app.use('/api',Routes)
 
 app.listen(PORT, ()=>{
